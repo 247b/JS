187557ce17a8f498b47fc4e7eeb37514d2ec72ed
247b <114521419+247b@users.noreply.github.com>
2024-06-13 20:11:22 +0530

    13/6/24

---
+++ Rock-paper-scissors/index.js
@@ -0,0 +1,48 @@
+let randomNum;
+let computerMove = ''
+let playerMove =''
+let result =''
+
+//creates the computer's move using Math.random and stores in computerMove.
+function getComputerMove() {
+  randomNum = Math.floor(Math.random()*3+1)
+  if (randomNum === 1){
+    computerMove = 'rock🗿'
+  }else if(randomNum === 2){
+    computerMove = 'paper📃'
+  }else if(randomNum === 3){
+    computerMove = 'scissors✂'
+  }
+}
+
+// decides the result of the game by passing player's move as myMove into the function and stores in result variable.
+function compare(myMove){
+  playerMove = myMove
+  if( myMove === computerMove){
+    result = 'match tied🤝'
+
+  }else if ((myMove === 'rock🗿' && computerMove === 'paper📃') || 
+            (myMove === 'paper📃' && computerMove === 'scissors✂') ||
+            (myMove === 'scissors✂' && computerMove === 'rock🗿')){
+    result = 'You lost the match👎'
+
+  }else if ((myMove === 'rock🗿' && computerMove === 'scissors✂') || 
+            (myMove === 'paper📃' && computerMove === 'rock🗿') ||
+            (myMove === 'scissors✂' && computerMove === 'paper📃')){
+    result = 'You won the match👍'
+  }
+}
+
+function alertBox() {
+  alert(`${result},\nYou picked ${playerMove},\nand computer picked ${computerMove}`)
+}
+
+//logs data in the console.
+function data(){
+  console.log(randomNum);
+  console.log(`You picked ${playerMove}`);
+  console.log(`computer picked ${computerMove}`);
+  console.log(result);
+}
+
+
